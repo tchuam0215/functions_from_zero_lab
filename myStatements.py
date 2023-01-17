693b2859502c50228ddaf2d8d4af1b83930c425a
kem's <42983505+tchuam0215@users.noreply.github.com>
2023-01-17 11:08:32 +0000

    adding files to repo

---
+++ myStatements.py
@@ -0,0 +1,14 @@
+
+my_list = ["Apple", "Cherry", "Orange", "Lemon"]
+
+my_dict = {"drink" : "coffe", "milk" : "whole", }
+
+for item in my_list : 
+    print(f"My favorite thing to eat is : {item}")
+
+# for _, value in my_dict : --> to drop the key 
+for key, value in my_dict.items() : 
+    print(f"My favorite {key} is {value} ")
+
+# my_list2 = 
+# my_dict = my_dict
